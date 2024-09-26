da177fcc1bb4f68f7fe1eba26cd499dafef7807f
Robert-Davie <robertjdavie2@gmail.com>
2024-09-26 22:00:17 +0100

    added more match cases

--- parser.py
+++ parser.py
@@ -1,63 +1,95 @@
 from typing import Optional
-
 from constants import response_codes, commands
-
 
 class SmtpParser:
     def __init__(self):
-        self.response_codes: list = []
-        self.commands_given: list = []
+        self.responses: list = []
+        self.commands: list = []
+
         self.server: str = ""
         self.client: str = ""
-        self.mail_from: str = ""
+
+        self.reverse_path: str = ""
         self.recipients: [str] = []
         self.data = [str]
-        self.server_ready: bool = False
+
         self.connection_active: bool = False
         self.authenticated: bool = False
         self.encrypted: bool = False
-        self.max_size: Optional[bool] = None
+        self.ehlo_info: {str: Optional[list | int]} = {}
         self.vrfy: {str: str} = {}
         self.expn: {str: list} = {}
-        self.speaking: Optional[str] = None
-        self.current_command = ""
-        self.current_arguments: [str] = []
-        self.save = True
+        self.email_queue_name: str = ""
 
-    def set_command(self, message: str | bytes) -> str:
+    def set_command(self, message: str | bytes) -> (str, str):
         if type(message) == bytes:
             message = message.decode("utf-8")
         command = message.split()[0].upper()
         if command in commands:
-            if self.save:
-                self.current_command = command
-                self.commands_given.append(command)
-            return command
+            self.current_command = command
+            self.commands.append(command)
+            return command, message[len(command):].strip()
         else:
             raise Exception(f"Command {command} Not Valid")
 
-    def set_mail_from(self, message: str) -> None:
-        elements = message.split()
-        if elements[2] == "<":
-            mail_from = elements[3]
-        else:
-            mail_from = elements[2].strip("<>.")
-        assert "@" in mail_from
-        self.mail_from = mail_from
+    def get_reverse_path(self, message: str) -> None:
+        start = message.index("<") + 1
+        end = message.index(">")
+        self.reverse_path = message[start:end].strip()
 
-    def set_save(self, save: bool) -> None:
-        self.save = save
+    def get_recipient(self, message: str) -> None:
+        start = message.index("<") + 1
+        end = message.index(">")
+        self.recipients.append(message[start:end].strip())
+
+    def resolve_command(self, response: str | bytes):
+        command, remainder = self.set_command(response)
+        match command:
+            case "HELO":
+                self.client = remainder.split()[0]
+            case "MAIL":
+                self.get_reverse_path(remainder)
+            case "RCPT":
+                self.get_recipient(remainder)
+            case "NOOP":
+                pass
+            case "QUIT":
+                pass
 
     def resolve_response(self, response: str | bytes):
-        response_code, response_message = self.response_to_code_and_message(response)
+        response_code, remainder = self.response_to_code_and_message(response)
+        last_command = self.last_command()
+
+        if response_code // 100 == 5:
+            match self.commands[-1]:
+                case "MAIL":
+                    self.reverse_path = ""
         match response_code:
             case 220:
-                self.server = response_message.split()[0]
+                self.server = remainder.split()[0]
                 self.server_ready = True
             case 221:
                 self.connection_active = False
+            case 250:
+                if last_command == "EHLO":
+                    remainder = remainder.strip("-").split()
+                    if len(remainder) == 1:
+                        self.ehlo_info[remainder[0]] = None
+                    elif len(remainder) > 1:
+                        self.ehlo_info[remainder[0]] = remainder[1:]
+            case 354:
+                pass
+            case 452:
+                if last_command == "RCPT":
+                    self.recipients.pop()
             case _:
                 pass
+
+    def last_command(self) -> Optional[str]:
+        if len(self.commands) > 0:
+            return self.commands[-1]
+        else:
+            return None
 
     def get_response_code(self, response: str | bytes) -> int:
         response_code, _ = self.response_to_code_and_message(response)
@@ -73,8 +105,7 @@ class SmtpParser:
         try:
             response_code = int(message[:3])
             response_message = message[3:].strip()
-            if self.save:
-                self.response_codes.append(response_code)
+            self.responses.append(response_code)
             return response_code, response_message
         except ValueError:
             raise
--- test_parser.py
+++ test_parser.py
@@ -30,7 +30,7 @@ def test_get_all_response_codes():
     p = SmtpParser()
     p.response_to_code_and_message('250 OK\r\n')
     p.response_to_code_and_message('221 Bye\r\n')
-    assert p.response_codes == [250, 221]
+    assert p.responses == [250, 221]
 
 
 def test_get_response_code_no_code():
@@ -46,7 +46,7 @@ def test_get_meaning_of_250_code():
 
 def test_get_command_valid_command():
     p = SmtpParser()
-    assert p.set_command("EHLO bar.com") == "EHLO"
+    assert p.set_command("EHLO bar.com") == ("EHLO", "bar.com")
 
 
 def test_get_command_invalid_command():
@@ -57,14 +57,20 @@ def test_get_command_invalid_command():
 
 def test_get_mail_from():
     p = SmtpParser()
-    p.set_mail_from("MAIL FROM: < someone@gmail.com >")
-    assert p.mail_from == "someone@gmail.com"
+    p.get_reverse_path("MAIL FROM: < someone@gmail.com >")
+    assert p.reverse_path == "someone@gmail.com"
+
+
+def test_get_mail_from_alt_2():
+    p = SmtpParser()
+    p.get_reverse_path("MAIL FROM:<person@mail.com>")
+    assert p.reverse_path == "person@mail.com"
 
 
 def test_get_mail_from_alt_format():
     p = SmtpParser()
-    p.set_mail_from("MAIL FROM: <someone@cs.cf.ac.uk>.")
-    assert p.mail_from == "someone@cs.cf.ac.uk"
+    p.get_reverse_path("MAIL FROM: <someone@cs.cf.ac.uk>.")
+    assert p.reverse_path == "someone@cs.cf.ac.uk"
 
 
 def test_220_response():
@@ -73,7 +79,7 @@ def test_220_response():
     assert all([
         p.server == "foo.com",
         p.server_ready,
-        p.response_codes == [220],
+        p.responses == [220],
     ])
 
 
@@ -82,3 +88,65 @@ def test_221_response():
     p.connection_active = True
     p.resolve_response("221 foo.com Service closing transmission channel")
     assert not p.connection_active
+
+
+def test_set_client():
+    p = SmtpParser()
+    p.resolve_command("HELO client.example.com")
+    assert p.client == "client.example.com"
+
+
+def test_get_recipient():
+    p = SmtpParser()
+    p.resolve_command("RCPT TO:<person@mail.com>")
+    assert p.recipients == ["person@mail.com"]
+
+
+def test_bad_sequence_order():
+    p = SmtpParser()
+    p.resolve_response("220 server.example.com ESMTP")
+    p.resolve_command("MAIL FROM:<person@mail.com>")
+    p.resolve_response("503 5.5.2 Send hello first")
+    assert all([
+        p.server == "server.example.com",
+        p.commands == ["MAIL"],
+        p.responses == [220, 503],
+        p.reverse_path == "",
+    ])
+
+
+def test_response_code_with_hyphen():
+    p = SmtpParser()
+    p.resolve_response("250-EXPN")
+    assert p.responses == [250]
+
+
+def test_get_ehlo_info():
+    p = SmtpParser()
+    p.resolve_command("EHLO server.example.com")
+    p.resolve_response("250 - PIPELINING")
+    p.resolve_response("250 - SIZE 10240000")
+    p.resolve_response("250 - VRFY")
+    assert p.ehlo_info == {
+        "PIPELINING": None,
+        "SIZE": ["10240000"],
+        "VRFY": None,
+    }
+
+
+def test_ehlo_info_with_hyphen():
+    p = SmtpParser()
+    p.resolve_command("EHLO server.example.com")
+    p.resolve_response("250-X-LINK2STATE")
+    assert p.ehlo_info == {
+        "X-LINK2STATE": None
+    }
+
+
+def test_too_many_recipients():
+    p = SmtpParser()
+    p.resolve_command("RCPT TO:<person1@mail.com>")
+    p.resolve_response("250 OK")
+    p.resolve_command("RCPT TO:<person2@mail.com>")
+    p.resolve_response("452 4.5.3 Error: too many recipients")
+    assert p.recipients == ["person1@mail.com"]
